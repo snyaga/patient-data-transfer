17e522226d52f778da71ca346c3ec854a1bdf6fb
snyaga <wnwsanya@gmail.com>
2015-05-28 10:13:12 +0300

    first commit

---
+++ api/src/main/java/org/openmrs/module/patientdatatransfer/api/impl/PatientDataTransferServiceImpl.java
@@ -0,0 +1,44 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.patientdatatransfer.api.impl;
+
+import org.openmrs.api.impl.BaseOpenmrsService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.module.patientdatatransfer.api.PatientDataTransferService;
+import org.openmrs.module.patientdatatransfer.api.db.PatientDataTransferDAO;
+
+/**
+ * It is a default implementation of {@link PatientDataTransferService}.
+ */
+public class PatientDataTransferServiceImpl extends BaseOpenmrsService implements PatientDataTransferService {
+	
+	protected final Log log = LogFactory.getLog(this.getClass());
+	
+	private PatientDataTransferDAO dao;
+	
+	/**
+     * @param dao the dao to set
+     */
+    public void setDao(PatientDataTransferDAO dao) {
+	    this.dao = dao;
+    }
+    
+    /**
+     * @return the dao
+     */
+    public PatientDataTransferDAO getDao() {
+	    return dao;
+    }
+}
